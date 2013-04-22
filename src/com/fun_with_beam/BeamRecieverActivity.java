a06e15168418b1063f502b03c989623251660c4c
Tomer Weller <tomer.weller@gmail.com>
2013-04-22 18:51:24 +0300

    init

---
+++ src/com/fun_with_beam/BeamRecieverActivity.java
@@ -0,0 +1,53 @@
+package com.fun_with_beam;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.widget.TextView;
+
+/**
+ * User: tomerweller
+ * Date: 4/22/13
+ * Time: 2:46 PM
+ */
+public class BeamRecieverActivity extends Activity{
+    TextView textView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        textView = (TextView) findViewById(R.id.textview);
+        textView.setText("Waiting...");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Check to see that the Activity started due to an Android Beam
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        // onResume gets called after this to handle the intent
+        setIntent(intent);
+    }
+
+    /**
+     * Parses the NDEF Message from the intent and prints to the TextView
+     */
+    void processIntent(Intent intent) {
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                NfcAdapter.EXTRA_NDEF_MESSAGES);
+        // only one message sent during the beam
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        // record 0 contains the MIME type, record 1 is the AAR, if present
+        textView.setText(new String(msg.getRecords()[0].getPayload()));
+    }
+}
---
+++ src/com/fun_with_beam/BeamSenderActivity.java
@@ -0,0 +1,59 @@
+package com.fun_with_beam;
+
+import android.app.Activity;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.nfc.NdefRecord.createMime;
+
+/**
+ * User: tomerweller
+ * Date: 4/22/13
+ * Time: 2:46 PM
+ */
+public class BeamSenderActivity extends Activity implements NfcAdapter.CreateNdefMessageCallback {
+    NfcAdapter mNfcAdapter;
+    TextView textView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        textView = (TextView) findViewById(R.id.textview);
+        textView.setText("Sender");
+        // Check for available NFC Adapter
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        //TODO: also check if enabled: mNfcAdapter.isEnabled()
+
+        // Register callback
+        mNfcAdapter.setNdefPushMessageCallback(this, this);
+    }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+        String text = ("Beam me up, Android!\n\n" +
+                "Beam Time: " + System.currentTimeMillis());
+        NdefMessage msg = new NdefMessage(
+                new NdefRecord[] { createMime("application/com.fun_with_beam", text.getBytes())}
+        );
+        return msg;
+    }
+
+    /**
+     * The Android Application Record (AAR). Optional.
+     *
+     *,NdefRecord.createApplicationRecord("com.fun_with_beam")
+     **/
+
+}
